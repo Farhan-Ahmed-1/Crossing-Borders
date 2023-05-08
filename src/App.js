00773972e15d4d3dd928d4af810c94084afeee21
Farhan Ahmed <farhan.suffyan@gmail.com>
2023-05-08 22:48:56 +0530

    tailwind navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import logo from './images/Rotating_earth.gif';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello khade how are 
+      <Navbar/> 
+      <img src={logo} />
     </div>
   );
 }
