8fc0343a5c834b92e022ccb608e894c36520c96b
Abdul Khader Wali <abdul.khader5166@gmail.com>
2023-05-10 23:17:11 +0530

    SocialLinks error removed

--- src/components/SocialLInks.js
+++ src/components/SocialLInks.js
@@ -1,54 +1,54 @@
 import React from 'react'
 
 const SocialLInks = () => {
-  return (
-    <div>
-      <ul>
-  <li style= {{-i: 7, -clr: '#FF0000'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-youtube" /></span>
-      Youtube
-    </a>
-  </li>
-  <li style={{-i: 6, -clr: '#0088cc'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-telegram" /></span>
-      Telegram
-    </a>
-  </li>
-  <li style={{-i: 5, -clr: '#bd081c'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-pinterest" /></span>
-      Pinterest
-    </a>
-  </li>
-  <li style={{-i: 4, -clr: '#ee5207'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-instagram" /></span>
-      Instagram
-    </a>
-  </li>
-  <li style={{-i: 3, -clr: '#1877f2'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-facebook" /></span>
-      Facebook
-    </a>
-  </li>
-  <li style={{-i: 2, -clr: '#171515'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-github" /></span>
-      Github
-    </a>
-  </li>
-  <li style={{-i: 1, -clr: '#1DA1F2'}}>
-    <a href="#">
-      <span><i className="fa-brands fa-twitter" /></span>
-      Twitter
-    </a>
-  </li>
-</ul>
+    return (
+        <div>
+  <ul>
+    <li>
+      <a href="#">
+        <span><i className="fa-brands fa-youtube" /></span>
+        Youtube
+      </a>
+    </li>
+    <li >
+      <a href="#">
+        <span><i className="fa-brands fa-telegram" /></span>
+        Telegram
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span><i className="fa-brands fa-pinterest" /></span>
+        Pinterest
+      </a>
+    </li>
+    <li >
+      <a href="#">
+        <span><i className="fa-brands fa-instagram" /></span>
+        Instagram
+      </a>
+    </li>
+    <li >
+      <a href="#">
+        <span><i className="fa-brands fa-facebook" /></span>
+        Facebook
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span><i className="fa-brands fa-github" /></span>
+        Github
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span><i className="fa-brands fa-twitter" /></span>
+        Twitter
+      </a>
+    </li>
+  </ul>
+</div>
 
-   </div>
   )
 }
 
