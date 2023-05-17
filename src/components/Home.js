91a60ceb358fe28cad0a2accd4eb40b657029bbb
farhan <farhan.suffyan@gmail.com>
2023-05-17 12:04:12 +0530

    homepage updated, navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import logo from '../images/Rotating_earth.gif';
 import './Home.css'
 import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 
 const notify = (user) => {
   if(user && user.email){
@@ -17,11 +18,42 @@ const Home = ({user}) => {
   }, [])
   
   return (
-    <div className='Home'>
-      <img src={logo} />
-      <p>Hellto this is some text</p>
-      <Toaster />
+    <div className="Home">
+      <img className='rotating-earth' src={logo} />
+  <div className="content">
+    <h4 className='text-yellow-500 my-8'>Welcome to </h4>
+    <h1 className='title text-white text-6xl font-serif'>
+      Global <span className="moments text-blue-700">Movements</span>
+    </h1>
+    <h3 className='subtitle text-2xl text-white mb-3'>Search for a better life &amp; a New Beginning </h3>
+    <h5 className='para text-gray-300 mb-4'>
+      Migration is a phenomenon that has been a part of human history for
+      centuries. It involves the movement of people from one place to another,
+      often in search of better opportunities, safety, or a new beginning.
+      Whether it's economic migration, refugee migration, or cultural migration,
+      people have always been on the move. Migration can be both voluntary and
+      forced, and it often has significant impacts on both the individuals who
+      migrate and the communities they leave behind. Despite its challenges and
+      complexities, migration is an essential part of the human story and has
+      helped shape the world as we know it today.
+    </h5>
+    <div>
+      <button className="bottom text-purple-900 font-black bg-yellow-500 py-3 px-5 mr-20 rounded-xl hover:bg-yellow-300" type="button">
+        <span /> <Link to="/register">Regrister </Link>{" "}
+      </button>
+      <button className="bottom text-purple-900 font-black bg-yellow-500 py-3 px-6 rounded-xl hover:bg-yellow-300" type="button">
+        
+        {
+          user&&user.email ? <span> <Link to="/map">Explore </Link></span> :
+          <span> <Link to="/login">Explore </Link> </span>
+        }
+        {" "}
+      </button>
     </div>
+  </div>
+  <Toaster />
+</div>
+
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {BiWorld} from 'react-icons/bi'
 import {HiOutlineBars3BottomRight} from 'react-icons/hi2'
+import {FaHome, FaUserPlus} from 'react-icons/fa'
+import {IoLogOut} from 'react-icons/io5'
+import company from '../images/company.png'
 
 export default function Navbar({ user, setUser }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -15,16 +18,19 @@ export default function Navbar({ user, setUser }) {
     <>
       {/* <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"> */}
             {/* <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gradient-to-r from-emerald-800 via-yellow-300 to-sky-600"> */}
-                  <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gradient-to-r from-slate-900 to-slate-700">
+                  <nav className=" flex flex-wrap items-center justify-between bg-gradient-to-r from-slate-900 to-slate-700">
 
 
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="container flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               to="/"
             >
-              Crossing Borders
+              {/* Crossing Borders */}
+              <div className="flex items-center justify-center">
+                <img src={company} className="h-10 w-20 p-0 m-0"/><span>Global Movements</span>
+              </div>
             </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -36,12 +42,21 @@ export default function Navbar({ user, setUser }) {
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center" +
+              "ml-22 lg:flex flex-grow" +
               (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  to="/"
+                >
+                  <FaHome className="text-lg" />
+                  <span className="ml-2">Home</span>
+                </Link>
+              </li>
               <li className="nav-item">
                 {
                   user && user._id?
@@ -63,21 +78,14 @@ export default function Navbar({ user, setUser }) {
                 }
               </li>
               <li className="nav-item">
-                <Link
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  to="/whatsthis"
-                >
-                  <span className="ml-2">What's This?</span>
-                </Link>
-              </li>
-              <li className="nav-item">
                 {user && user._id ?
-                <button className="ml-2 px-3 py-2 cursor-pointer flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" onClick={handleLogout}>{`(${user.email}) `}Logout</button>
+                <button className="ml-2 px-3 py-2 font-black cursor-pointer flex items-center text-xs uppercase leading-snug text-white hover:opacity-75" onClick={handleLogout}>{`(${user.email}) `} <span className="mx-2">Logout</span> <IoLogOut className="text-xl" /></button>
                 :
                 <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   to="/login"
                 >
+                  <FaUserPlus className="text-lg" />
                   <span className="ml-2">Login</span>
                 </Link>
                 
